6c70db47096d54c65820bc822b95c98812ac08b8
Lys3rg1de <egormsm@gmail.com>
2021-03-19 11:29:55 +0300

    add: modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,61 @@
-const hotelSLider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  // keyboard control 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-});
-
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-  // keyboard control 
-  keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-  },
-});
-
-let menuButton = document.querySelector('.menu-button');
-  menuButton.addEventListener('click', () => {
-    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
+$(document).ready(() => {
+  const hotelSLider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    // keyboard control 
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
   });
+  
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+    // keyboard control 
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+  });
+  
+  const menuButton = document.querySelector('.menu-button');
+    menuButton.addEventListener('click', () => {
+      document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
+    });
+  
+    const modalButton = $("[data-toggle=modal]");
+    const closeModalButton = $(".modal__close");
+    const modalOverlay = $(".modal__overlay");
+    const modalDialog = $(".modal__dialog");
+    const openModal = () => {
+      modalOverlay.addClass("modal__overlay--visible");
+      modalDialog.addClass("modal__dialog--visible");
+    };
+    const closeModal = event => {
+      event.preventDefault();
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+    modalButton.on('click', openModal);
+    closeModalButton.on('click', closeModal);
+
+    $(document).keydown(event => {
+      const key = event.key
+      if (key === "Escape") {
+        event.preventDefault();
+        modalOverlay.removeClass("modal__overlay--visible");
+        modalDialog.removeClass("modal__dialog--visible");
+      }
+    });
+});
