7e6058a25ff93093ac349cd866590711b5f6c834
Lys3rg1de <egormsm@gmail.com>
2021-03-22 17:47:37 +0300

    add validation for forms

--- src/js/main.js
+++ src/js/main.js
@@ -29,9 +29,18 @@ $(document).ready(() => {
     },
   });
   
-  const menuButton = document.querySelector('.menu-button');
+  const menuButton = document.querySelector('.menu-button'),
+    navbarBottom = document.querySelector('.navbar-bottom');
     menuButton.addEventListener('click', () => {
-      document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible')
+      if (navbarBottom.classList.contains('navbar-bottom--visible')) {
+        navbarBottom.classList.remove('navbar-bottom--visible');
+        $("body").css("overflow","scroll");
+        bodyUnfixPosition();
+      } else {
+      navbarBottom.classList.add('navbar-bottom--visible');
+      $("body").css("overflow","hidden");
+      bodyFixPosition();
+      }
     });
   
     const modalButton = $("[data-toggle=modal]");
@@ -41,21 +50,97 @@ $(document).ready(() => {
     const openModal = () => {
       modalOverlay.addClass("modal__overlay--visible");
       modalDialog.addClass("modal__dialog--visible");
+      $("body").css("overflow","hidden");
+      bodyFixPosition();
     };
     const closeModal = event => {
       event.preventDefault();
       modalOverlay.removeClass("modal__overlay--visible");
       modalDialog.removeClass("modal__dialog--visible");
-    }
-    modalButton.on('click', openModal);
-    closeModalButton.on('click', closeModal);
-
+      $("body").css("overflow","scroll");
+      bodyUnfixPosition();
+    };
+    const turnModal = () => {
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+      $("body").css("overflow","scroll");
+      bodyUnfixPosition();
+    };
     $(document).keydown(event => {
-      const key = event.key
+      const key = event.key;
       if (key === "Escape") {
-        event.preventDefault();
-        modalOverlay.removeClass("modal__overlay--visible");
-        modalDialog.removeClass("modal__dialog--visible");
+        turnModal();
       }
     });
+    $(document).click(event => {
+      if ($(event.target).is('.modal__overlay')) {
+        turnModal();
+      }
+    });
+    modalButton.on('click', openModal);
+    closeModalButton.on('click', closeModal);
+    const bodyFixPosition = () => {
+
+      setTimeout(() => {
+      /* Ставим необходимую задержку, чтобы не было «конфликта» в случае, если функция фиксации вызывается сразу после расфиксации (расфиксация отменяет действия расфиксации из-за одновременного действия) */
+        if ( !document.body.hasAttribute('data-body-scroll-fix') ) {
+
+          // Получаем позицию прокрутки
+          let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    
+          // Ставим нужные стили
+          document.body.setAttribute('data-body-scroll-fix', scrollPosition); // Cтавим атрибут со значением прокрутки
+          document.body.style.overflow = 'hidden';
+          document.body.style.position = 'fixed';
+          document.body.style.top = '-' + scrollPosition + 'px';
+          document.body.style.left = '0';
+          document.body.style.width = '100%';
+    
+        }
+    
+      }, 15 ); /* Можно задержку ещё меньше, но у меня работало хорошо именно с этим значением на всех устройствах и браузерах */
+    
+    }
+    
+    // 2. Расфиксация <body>
+    function bodyUnfixPosition() {
+    
+      if ( document.body.hasAttribute('data-body-scroll-fix') ) {
+    
+        // Получаем позицию прокрутки из атрибута
+        let scrollPosition = document.body.getAttribute('data-body-scroll-fix');
+    
+        // Удаляем атрибут
+        document.body.removeAttribute('data-body-scroll-fix');
+    
+        // Удаляем ненужные стили
+        document.body.style.overflow = '';
+        document.body.style.position = '';
+        document.body.style.top = '';
+        document.body.style.left = '';
+        document.body.style.width = '';
+    
+        // Прокручиваем страницу на полученное из атрибута значение
+        window.scroll(0, scrollPosition);
+      }
+    }
+    $(".form").each(function() {
+      $(this).validate({
+        errorClass: "invalid",
+        messages: {
+          name: {
+            required: "Please specify your name",
+            minlength: "The name must be at least 2 characters long "
+          },
+          email: {
+            required: "We need your email address to contact you",
+            email: "Your email address must be in the format of name@domain.com"
+          },
+          phone: {
+            required: "Please specify your phone",
+          },
+        }
+      });
+    });
+    $('.phone').mask('+7 (000) 000-00-00');
 });
